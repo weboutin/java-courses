54f1b6debfc6a8816ab8ab476f273c426055526b
wuzhicong <wuzhicong@aiyunxiao.com>
2020-12-23 23:45:42 +0800

    feat: 实现restful的几个HTTP方法

--- course3/web/HelloWorld.java
+++ course3/web/HelloWorld.java
@@ -1,33 +1,78 @@
 // 导入必需的 java 库
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 // 扩展 HttpServlet 类
 public class HelloWorld extends HttpServlet {
- 
-  private String message;
 
-  public void init() throws ServletException
-  {
-      // 执行必需的初始化
-      message = "Hello World";
-  }
+    private String message;
 
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-            throws ServletException, IOException
-  {
-      // 设置响应内容类型
-      response.setContentType("text/html");
+    public void init() throws ServletException {
+        // 执行必需的初始化
+        message = "Hello World!!!";
+    }
 
-      // 实际的逻辑是在这里
-      PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
-  }
-  
-  public void destroy()
-  {
-      // 什么也不做
-  }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "Handle get" + "</h1>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "Handle post" + "</h1>");
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "Handle put" + "</h1>");
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "Handle delete" + "</h1>");
+    }
+
+    public void doPatch(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("Handle patch123");
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("PATCH")) {
+            doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
+    
+    public void destroy() {
+        // 什么也不做
+    }
+
+
 }
